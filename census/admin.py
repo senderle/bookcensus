649b1046496d0b2b9356d3383ee03b65a9728ca3
Scott Enderle <scott.enderle@gmail.com>
2018-06-08 15:17:20 +0000

    new model structure

--- census/admin.py
+++ census/admin.py
@@ -1,47 +1,23 @@
 from django.contrib import admin
+from django_extensions.admin import ForeignKeyAutocompleteAdmin
 
 # Register your models here.
 from .models import *
 
-admin.site.register(Title)
-admin.site.register(Edition)
-admin.site.register(UserProfile)
-admin.site.register(Issue)
-admin.site.register(Entity)
-admin.site.register(Transaction)
-admin.site.register(UserDetail)
-admin.site.register(CopyHistory)
-admin.site.register(ChildCopy)
 admin.site.register(StaticPageText)
+admin.site.register(Title)
+admin.site.register(Issue)
+admin.site.register(Edition)
+admin.site.register(Copy)
+admin.site.register(UserProfile)
+admin.site.register(UserDetail)
 
-class BookPlatezInline(admin.StackedInline):
-	model = BookPlate
-	fk_name = 'copy'
-class BookPlateLocationInline(admin.StackedInline):
-	model = BookPlate_Location
-	fk_name = 'copy'
-class TransferInline(admin.StackedInline):
-	model = Transfer
-	fk_name = 'copy'
-class TransferValueInline(admin.StackedInline):
-	model = Transfer_Value
-	fk_name = 'copy'
-class ProvenanceInline(admin.StackedInline):
-	model = Provenance
-	fk_name = 'copy'
+@admin.register(DraftCopy)
+class DraftCopyAdmin(admin.ModelAdmin):
+    raw_id_fields = ("parent",)
 
-class CopyAdmin(admin.ModelAdmin):
-	inlines = [
-		 BookPlatezInline,
-		 BookPlateLocationInline,
-		 TransferInline,
-		 TransferValueInline,
-		 ProvenanceInline,
-	]
-
-admin.site.register(Copy, CopyAdmin)
-admin.site.register(BookPlate)
-admin.site.register(BookPlate_Location)
-admin.site.register(Transfer)
-admin.site.register(Transfer_Value)
-admin.site.register(Provenance)
+    
+admin.site.register(CanonicalCopy)
+admin.site.register(HistoryCopy)
+admin.site.register(FalseCopy)
+admin.site.register(BaseCopy)
--- census/models.py
+++ census/models.py
@@ -6,6 +6,43 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 # from tinymce import models as tinymce_models
 
+### Main Site Operations ###
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    def __unicode__(self):
+        return self.user.username
+    def delete(self, *args, **kwargs):
+        self.user.delete()
+        return super(UserProfile,self).delete(*args, **kwargs)
+
+class UserDetail(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    affiliation = models.CharField(max_length=255, null=True)
+    group=models.ForeignKey(Group, default=1)
+
+    def __str__(self):
+        return self.user.username
+    class Meta:
+        verbose_name_plural = "user details"
+
+class StaticPageText(models.Model):
+    content = models.TextField(null=True, blank=True, default=None)
+    #htmlcontent = tinymce_models.HTMLField()
+    viewname = models.CharField(max_length=255, default='', null=True, blank=True)
+
+    def __str__(self):
+        return self.viewname
+    class Meta:
+        verbose_name_plural = "Static Pages"
+
+@receiver(post_save, sender=User)
+def create_user_detail(sender, instance, created, **kwargs):
+    if created:
+        UserDetail.objects.create(user=instance)
+
+### Core Data Tables ###
+
 class Title(models.Model):
     title = models.CharField(max_length=128, unique=True)
     Apocryphal = models.BooleanField(default=False)
@@ -33,7 +70,137 @@ class Issue (models.Model):
     def __str__(self):
         return "%s ESTC %s" % (self.edition, self.ESTC)
 
-class Copy (models.Model):
+# Essential fields for all copies.
+class BaseCopy(models.Model):
+    Owner = models.CharField(max_length=500)
+    issue = models.ForeignKey(Issue, unique=False)
+    thumbnail_URL = models.URLField(max_length=500, null=True, blank=True)
+    NSC = models.IntegerField(default=0, null=True)
+    Shelfmark = models.CharField(max_length=500, default=None, null=True, blank=True)
+    Height = models.FloatField(default=0, null=True)
+    Width = models.FloatField(default=0, null=True)
+    Marginalia = models.TextField(null=True, blank=True, default=None)
+    Condition = models.CharField(max_length=200, default=None, null=True, blank=True)
+    Binding = models.CharField(max_length=200, default=None, null=True, blank=True)
+    Binder = models.CharField(max_length=40, default=None, null=True, blank=True)
+    Bookplate = models.CharField(max_length=40, default=None, null=True, blank=True)
+    Bookplate_Location = models.CharField(max_length=100, default=None, null=True, blank=True)
+    Bartlett1939 = models.IntegerField(default=0, null=True)
+    Bartlett1939_Notes = models.TextField(null=True, blank=True, default=None)
+    Bartlett1916 = models.IntegerField(default=0, null=True)
+    Bartlett1916_Notes = models.TextField(null=True, blank=True, default=None)
+    Lee_Notes = models.TextField(null=True, blank=True, default=None)
+    Local_Notes = models.TextField(null=True, blank=True, default=None)
+    created_by = models.ForeignKey(User, related_name="user_submitted_copies", default=1, null=True)
+    prov_info = models.TextField(null=True, blank=True, default=None)
+    bibliography = models.TextField(null=True, blank=True, default=None)
+    from_estc = models.BooleanField(default=False)
+    location_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return  "%s (%s)" % (self.issue, self.issue.year)
+    class Meta:
+        verbose_name_plural = "Base copies"
+
+# Copy records that have been ruled spurious. These records should
+# be preserved across versions of the app if possible.
+class FalseCopy(BaseCopy):
+    class Meta:
+        verbose_name_plural = "False copies"
+
+# Copy records that we are treating as accurate. These records *must*
+# be preserved across versions of the app.
+class CanonicalCopy(BaseCopy):
+    class Meta:
+        verbose_name_plural = "Canonical copies"
+
+# Copy records that are modified versions of records from CanonicalCopy.
+# These are working records and historical versions that we don't treat
+# as vital for preservation.
+class DraftCopy(BaseCopy):
+    parent = models.ForeignKey(CanonicalCopy, related_name='drafts', default=None, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = "Draft copies"
+
+class HistoryCopy(BaseCopy):
+    parent = models.ForeignKey(CanonicalCopy, related_name='versions', default=None, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = "History copies"
+
+
+### Copy Management Classes/Callables ###
+
+class LinkedCopyCreate(object):
+    def __init__(self, source_model, target_model, base_model):
+        self.source_model = source_model
+        self.target_model = target_model
+        self.base_model = base_model
+        self.copy_fields = set(f.name for f in base_model._meta.fields)
+        self.copy_fields.discard('id')
+        self.copy_fields.discard('pk')
+    def __call__(self, source):
+        if not isinstance(source, self.source_model):
+            raise ValueError('Can only copy instances of {}'.format(self.source_model))
+        new = self.target_model()
+        for f in self.copy_fields:
+            setattr(new, f, getattr(source, f))
+        new.parent = source
+        new.save()
+
+create_draft = LinkedCopyCreate(CanonicalCopy, DraftCopy, BaseCopy)
+create_history = LinkedCopyCreate(CanonicalCopy, HistoryCopy, BaseCopy)
+
+class LinkedCopyUpdate(object):
+    def __init__(self, source_model, target_model, record_model, base_model):
+        self.source_model = source_model
+        self.target_model = target_model
+        self.record_model = record_model
+        self.base_model = base_model
+        self.create_record = LinkedCopyCreate(target_model, record_model, base_model)
+        self.copy_fields = set(f.name for f in base_model._meta.fields)
+        self.copy_fields.discard('id')
+        self.copy_fields.discard('pk')
+    def __call__(self, source):
+        if not isinstance(source, self.source_model):
+            raise ValueError('Can only update from instances of {}'.format(self.source_model))
+        if not isinstance(source.parent, self.target_model):
+            raise ValueError('Can only update to instances of {}, but the parent of {} is a {}'.format(self.target_model, source, type(source.parent)))
+        parent = source.parent
+        self.create_record(parent)
+        for f in self.copy_fields:
+            setattr(parent, f, getattr(source, f))
+        parent.save()
+        source.delete()
+
+draft_to_canonical_update = LinkedCopyUpdate(DraftCopy, CanonicalCopy, HistoryCopy, BaseCopy)
+history_to_canonical_update = LinkedCopyUpdate(HistoryCopy, CanonicalCopy, DraftCopy, BaseCopy)
+
+class LinkedCopyCreateParent(object):
+    def __init__(self, source_model, target_model, base_model):
+        self.source_model = source_model
+        self.target_model = target_model
+        self.base_model = base_model
+        self.copy_fields = set(f.name for f in base_model._meta.fields)
+        self.copy_fields.discard('id')
+        self.copy_fields.discard('pk')
+    def __call__(self, source):
+        if not isinstance(source, self.source_model):
+            raise ValueError('Can only copy instances of {}'.format(self.source_model))
+        if source.parent:
+            raise ValueError('Can only create parents for objects that don\'t have one!')
+        new = self.target_model()
+        for f in self.copy_fields:
+            setattr(new, f, getattr(source, f))
+        source.parent = new
+        new.save()
+
+draft_to_canonical_create = LinkedCopyCreateParent(DraftCopy, CanonicalCopy, BaseCopy)
+
+### Old Models (Unused or to be retired) ###
+
+class Copy(models.Model):
     Owner = models.CharField(max_length=500)
     issue = models.ForeignKey(Issue, unique=False)
     thumbnail_URL = models.URLField(max_length=500, null=True, blank=True)
@@ -77,14 +244,7 @@ class CopyHistory(Copy):
     stored_copy = models.ForeignKey(Copy, related_name="copy_history", default=None, null=True)  #copy of which the current object is the history of
     date_created = models.DateTimeField(auto_now_add=True)
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    def __unicode__(self):
-        return self.user.username
-    def delete(self, *args, **kwargs):
-        self.user.delete()
-        return super(UserProfile,self).delete(*args, **kwargs)
-
+# (Currently Unused)
 class Entity(models.Model):
     name = models.CharField(max_length=200)
     notes = models.CharField(max_length=500)
@@ -93,6 +253,7 @@ class Entity(models.Model):
     class Meta:
         verbose_name_plural = "entities"
 
+# (Currently Unused)
 class Transaction(models.Model):
     copy = models.ForeignKey(Copy, on_delete=models.CASCADE)
     buyer = models.ForeignKey(Entity, related_name="entity_buyer", max_length=100)
@@ -101,29 +262,6 @@ class Transaction(models.Model):
     price = models.DecimalField(max_digits = 10, decimal_places=2)
     def __str__(self):
         return "Sold from %s to %s" % (self.seller, self.buyer)
-
-class UserDetail(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    affiliation = models.CharField(max_length=255, null=True)
-    group=models.ForeignKey(Group, default=1)
-
-    def __str__(self):
-        return self.user.username
-    class Meta:
-        verbose_name_plural = "user details"
-
-class StaticPageText(models.Model):
-    content = models.TextField(null=True, blank=True, default=None)
-    #htmlcontent = tinymce_models.HTMLField()
-    viewname = models.CharField(max_length=255, default='', null=True, blank=True)
-
-    def __str__(self):
-        return self.viewname
-
-@receiver(post_save, sender=User)
-def create_user_detail(sender, instance, created, **kwargs):
-    if created:
-        UserDetail.objects.create(user=instance)
 
 #The following models are not used right now. Need further information.
 class Provenance (models.Model):
