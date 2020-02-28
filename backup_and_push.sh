eeb846067f173272781e57936dbbbdf7e958a357
Scott Enderle <scott.enderle@gmail.com>
2020-02-28 16:43:57 +0000

    automatically push backups

--- backup_and_push.sh
+++ backup_and_push.sh
@@ -3,6 +3,6 @@ chmod -R o+w census/ingest/data/json-backups
 sudo docker-compose -f production.yml run --rm django python3 manage.py exportjson census/ingest/data/json-backups/latest/`date +'%Y-%m-%d'`
 sudo chown -R senderle:senderle census/ingest/data
 chmod -R o-w census/ingest/data/json-backups
-# git add census/ingest/data/json-backups/latest*
-# git commit -m "latest backup"
-# git push
+git add census/ingest/data/json-backups/latest*
+git commit -m "latest backup"
+git push
