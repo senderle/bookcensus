c5923d2154c2fb7aa487142a3c8b0cc24284debf
Scott Enderle <scott.enderle@gmail.com>
2020-02-28 16:43:31 +0000

    streamline backup process

--- backup_and_push.sh
+++ backup_and_push.sh
@@ -1,4 +1,8 @@
+mv -f census/ingest/data/json-backups/latest/* census/ingest/data/json-backups/dated/
 chmod -R o+w census/ingest/data/json-backups
-sudo docker-compose -f production.yml run --rm django python3 manage.py exportjson census/ingest/data/json-backups/latest
+sudo docker-compose -f production.yml run --rm django python3 manage.py exportjson census/ingest/data/json-backups/latest/`date +'%Y-%m-%d'`
 sudo chown -R senderle:senderle census/ingest/data
 chmod -R o-w census/ingest/data/json-backups
+# git add census/ingest/data/json-backups/latest*
+# git commit -m "latest backup"
+# git push
