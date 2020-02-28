d664df46a9b398c4f7dbd68e8ddc3179e326a0ec
Scott Enderle <scott.enderle@gmail.com>
2020-02-28 17:30:34 +0000

    comment backup code

--- backup_and_push.sh
+++ backup_and_push.sh
@@ -1,8 +1,21 @@
+# Move most recent data to archive.
 mv -f census/ingest/data/json-backups/latest/* census/ingest/data/json-backups/dated/
+
+# Make folder writable by anyone. This is the easiest way to work around
+# the problem that the container's django user doesn't have write privileges.
 chmod -R o+w census/ingest/data/json-backups
+
+# Run the backup routine in a disposable container.
 sudo docker-compose -f production.yml run --rm django python3 manage.py exportjson census/ingest/data/json-backups/latest/`date +'%Y-%m-%d'`
-sudo chown -R senderle:senderle census/ingest/data
+
+# Files will be saved under the django user's UID, which locally translates
+# to some weird system account. So change back to current user.
+sudo chown -R $USER:$USER census/ingest/data/json-backups
+
+# Make folder read-only again for other users.
 chmod -R o-w census/ingest/data/json-backups
-git add census/ingest/data/json-backups/latest*
+
+# Commit and push changes to git.
+git add census/ingest/data/json-backups
 git commit -m "latest backup"
 git push
